d97714524efabd353d9b81f9a0cfc5548f6ae6df
Gareth Lloyd <gaz@weirdgloop.org>
2026-05-24 14:34:35 +0100

    significantly update equipment scrape to include everything, skip lower tiers and cosmetics, etc; add to gh worker so we can cron it on the server

--- scripts/generateEquipment.py
+++ scripts/generateEquipment.py
@@ -11,44 +11,35 @@
 import os
 import requests
 import json
-import urllib.parse
 import sys
+import time
 
-FILE_DIR = '../cdn/json'
+#FILE_DIR = '../cdn/json'
+FILE_DIR = '.'
 FILE_NAME = f'{FILE_DIR}/equipment.json'
+FILE_NAME_m = f'{FILE_DIR}/equipment_names_mainslots.txt'
+FILE_NAME_w = f'{FILE_DIR}/equipment_names_weapons.txt'
+FILE_NAME_o = f'{FILE_DIR}/equipment_names_other.txt'
 WIKI_BASE = 'https://runescape.wiki'
 API_BASE = WIKI_BASE + '/api.php'
-IMG_PATH = '../cdn/equipment/'
+IMG_PATH = './images/equipment/'
 
 BUCKET_API_FIELDS = [
     'page_name',
     'page_name_sub',
-    'item_name',
-    'image',
-    'item_id',
-    'version_anchor',
-    'infobox_bonuses.combat_class',
-    'infobox_bonuses.equipment_slot',
-    'infobox_bonuses.equipment_type',
-    'infobox_bonuses.weapon_damage',
-    'infobox_bonuses.weapon_accuracy',
-    'infobox_bonuses.attack_style',
-    'infobox_bonuses.attack_range',
-    'infobox_bonuses.equipment_armour',
-    'infobox_bonuses.equipment_life_points',
-    'infobox_bonuses.weapon_attack_speed',
-    'infobox_bonuses.prayer_bonus',
-    'infobox_bonuses.strength_bonus',
-    'infobox_bonuses.ranged_bonus',
-    'infobox_bonuses.magic_bonus',
-    'infobox_bonuses.necromancy_bonus',
-    'infobox_bonuses.equipment_tier',
-    'infobox_bonuses.pvm_damage_reduction',
-    'infobox_bonuses.pvp_damage_reduction',
+    'infobox_bonuses.json',
+    'infobox_item.item_name',
+    'infobox_item.image',
+    'infobox_item.item_id',
+    'infobox_item.version_anchor',
+    'infobox_item.location_restriction'
 ]
 
 ITEMS_TO_SKIP = [
 ]
+
+s = requests.Session()
+s.headers.update({'User-Agent': 'rs-rotations (https://github.com/weirdgloop/rs-rotations)'})
 
 def try_float(x, defl=0):
     try:
@@ -72,19 +63,20 @@ def getEquipmentData():
             'format': 'json',
             'query': 
             (
-                f"bucket('infobox_item')"
+                f"bucket('infobox_bonuses')"
+                f".join('infobox_item', 'infobox_bonuses.page_name_sub', 'infobox_item.page_name_sub')"
                 f".select({fields_csv})"
                 f".limit(500).offset({offset})"
                 f".where('infobox_bonuses.equipment_slot', '!=', bucket.Null())"
-                f".where('item_id', '!=', bucket.Null())"
                 f".where('infobox_bonuses.is_cosmetic_recolour', '!=', true)"
-                ".where(bucket.Not({'infobox_item.location_restriction', '!=', bucket.Null()}))"
-                f".join('infobox_bonuses', 'infobox_bonuses.page_name_sub', 'infobox_item.page_name_sub')"
+                #f".where(bucket.Not('Category:Recurring content'))"
+                #".where(bucket.Not({'infobox_item.location_restriction', '!=', bucket.Null()}))"
                 f".orderBy('page_name_sub', 'asc').run()"
             )
         }
 
-        r = requests.get(API_BASE + '?' + urllib.parse.urlencode(query), headers={
+        time.sleep(0.2)
+        r = s.get(API_BASE, params=query, headers={
             'User-Agent': 'rs-rotations (https://github.com/weirdgloop/rs-rotations)'
         })
         #print(r.url)
@@ -104,16 +96,70 @@ def getEquipmentData():
             # If we are at the end of the results, break out of this loop
             break
 
-    return equipment
+    items = {}
+    offset = 0
+    while True:
+        print('Fetching item info: ' + str(offset))
+        query = {
+            'action': 'bucket',
+            'format': 'json',
+            'query': 
+            (
+                f"bucket('infobox_item')"
+                f".select('page_name','page_name_sub','image','item_id','version_anchor','location_restriction')"
+                f".limit(500).offset({offset})"
+                f".where('item_id', '!=', bucket.Null())"
+                f".where('Category:Equipment')"
+                f".orderBy('page_name_sub', 'asc').run()"
+            )
+        }
 
+        time.sleep(0.2)
+        r = s.get(API_BASE, params=query, headers={
+            'User-Agent': 'rs-rotations (https://github.com/weirdgloop/rs-rotations)'
+        })
+        #print(r.url)
+
+        data = r.json()
+
+        if 'bucket' not in data:
+            # No results?
+            break
+
+        for val in data['bucket']:
+            pn = val['page_name']
+            if pn in items:
+                items[pn].append(val)
+            else:
+                items[pn] = [val]
+        # Bucket's API doesn't tell you when there are more results, so we'll just have to guess
+        if len(data['bucket']) == 500:
+            offset += 500
+        else:
+            # If we are at the end of the results, break out of this loop
+            break
+    
+    return equipment, items
+
+
+BONUS_PARAMS = frozenset(['damage', 'accuracy', 'armour', 'life', 'speed', 'prayer', 'strength', 'ranged', 'magic', 'necromancy', 'tier'])
+TIER_SLOTS = frozenset(['head', 'torso', 'legs', 'hands', 'feet', 'main hand weapon', 'off-hand weapon', '2h weapon', 'off-hand', 'ammo'])
+OK_RESTRICTION = frozenset([None, 'surface'])
+
+def tryint(x):
+    try:
+        return int(x)
+    except ValueError:
+        return None
 
 def main(get_images=False):
     # Grab the equipment info using Bucket
-    wiki_data = getEquipmentData()
+    wiki_data, item_data = getEquipmentData()
 
     # Use an object rather than an array, so that we can't have duplicate items with the same page_name_sub
     data = {}
     required_imgs = []
+    dedupe = {}
 
     # Loop over the equipment data from the wiki
     for v in wiki_data:
@@ -122,50 +168,96 @@ def main(get_images=False):
 
         print(f"Processing {v['page_name_sub']}")
 
+        iteminfo = item_data.get(v['page_name'])
         try:
-            item_id = int(v.get('item_id')[0]) if v.get('item_id') else None
+            item_ids = list(map(int, v.get('infobox_item.item_id'))) if 'infobox_item.item_id' in v else None
         except ValueError:
             # Item has an invalid ID, do not show it here as it's probably historical or something.
             print("Skipping - invalid item ID (not an int)")
             continue
 
+        itemname = v['page_name']
+        itemname_sub = v['page_name_sub']
+
+        if itemname_sub in data:
+            continue
+
+        if itemname.startswith('Augmented'):
+            continue
+
+        if itemname in ITEMS_TO_SKIP:
+            continue
+
+        img = []
+        if 'infobox_item.item_name' in v:
+            # successful join
+            if v.get('infobox_item.location_restriction', 'surface') not in OK_RESTRICTION:
+                continue
+            imgs = v.get('infobox_item.image')
+            if imgs is not None:
+                img = list(map(lambda x: x.replace('File:', ''), imgs))
+        else:
+            if iteminfo is None:
+                continue
+            #failed join
+            imgs = []
+            item_ids = []
+            bad_lr = False
+            for item in iteminfo:
+                if item.get('location_restriction', 'surface') not in OK_RESTRICTION:
+                    bad_lr = True
+                    break
+                imgs += item.get('image')
+                item_ids += list(filter(lambda x: x is not None, map(tryint, item.get('item_id'))))
+            if bad_lr:
+                continue
+            img = list(map(lambda x: x.replace('File:', ''), imgs))
+        
+        img.sort(key=len)
+        if item_ids is not None:
+            item_ids.sort()
+
+        bonuses = json.loads(v['infobox_bonuses.json'])
+
         equipment = {
-            'name': v['page_name'],
-            'id': item_id,
-            'version': v.get('version_anchor', ''),
-            'image': '' if not v.get('image') else v.get('image')[-1].replace('File:', ''),
+            'name': itemname,
+            'id': item_ids,
+            'version': v.get('infobox_item.version_anchor', ''),
+            'image': img,
             'bonuses': {
-                'class': v.get('infobox_bonuses.combat_class', ''),
-                'slot': v.get('infobox_bonuses.equipment_slot', ''),
-                'type': v.get('infobox_bonuses.equipment_type', ''),
-                'damage': try_float(v.get('infobox_bonuses.weapon_damage', '')),
-                'accuracy': try_int(v.get('infobox_bonuses.weapon_accuracy', '')),
-                'style': v.get('infobox_bonuses.attack_style', ''),
-                'attack_range': try_int(v.get('infobox_bonuses.attack_range', '')),
-                'armour': try_float(v.get('infobox_bonuses.equipment_armour', '')),
-                'life': v.get('infobox_bonuses.equipment_life_points', 0),
-                'speed': try_int(v.get('infobox_bonuses.weapon_attack_speed', '')),
-                'prayer': v.get('infobox_bonuses.prayer_bonus', 0),
-                'strength': v.get('infobox_bonuses.strength_bonus', 0),
-                'ranged': v.get('infobox_bonuses.ranged_bonus', 0),
-                'magic': v.get('infobox_bonuses.magic_bonus', 0),
-                'necromancy': v.get('infobox_bonuses.necromancy_bonus', 0),
-                'tier': v.get('infobox_bonuses.equipment_tier', ''),
-                'pvm_reduction': v.get('infobox_bonuses.pvm_damage_reduction', ''),
-                'pvp_reduction': v.get('infobox_bonuses.pvp_damage_reduction', ''),
+                'class': bonuses.get('class', ''),
+                'slot': bonuses.get('slot', ''),
+                'type': bonuses.get('type', ''),
+                'damage': try_float(bonuses.get('damage', '')),
+                'ability_damage': try_float(bonuses.get('ability_damage', '')),
+                'accuracy': try_int(bonuses.get('accuracy', '')),
+                'style': bonuses.get('style', ''),
+                'attack_range': try_int(bonuses.get('attack_range', '')),
+                'armour': try_float(bonuses.get('armour', '')),
+                'life': bonuses.get('lp', 0),
+                'speed': try_int(bonuses.get('speed', '')),
+                'prayer': bonuses.get('prayer', 0),
+                'strength': bonuses.get('strength', 0),
+                'ranged': bonuses.get('ranged', 0),
+                'magic': bonuses.get('magic', 0),
+                'necromancy': bonuses.get('necromancy', 0),
+                'tier': bonuses.get('tier', ''),
+                #'pvm_reduction': bonuses.get('infobox_bonuses.pvm_damage_reduction', ''),
+                #'pvp_reduction': bonuses.get('infobox_bonuses.pvp_damage_reduction', ''),
             }
         }
         is_cosmetic = True
-        for bon in ['damage', 'accuracy', 'armour', 'life', 'speed', 'prayer', 'strength', 'ranged', 'magic', 'necromancy', 'tier']:
+        for bon in BONUS_PARAMS:
             is_cosmetic &= equipment['bonuses'].get(bon) == 0
         if is_cosmetic:
             continue
-
-        if equipment['name'] in ITEMS_TO_SKIP:
-            continue
+        if equipment['bonuses']['slot'] in TIER_SLOTS:
+            t = equipment['bonuses'].get('tier')
+            if (type(t) == int or type(t) == float) and t < 70:
+                continue
 
         # Set the current equipment item to the calc's equipment list
-        data[v['page_name_sub']] = equipment
+        data[itemname_sub] = equipment
 
         if not equipment['image'] == '':
             required_imgs.append(equipment['image'])
@@ -186,7 +278,25 @@ def main(get_images=False):
     with open(FILE_NAME, 'w') as f:
         print('Saving to JSON at file: ' + FILE_NAME)
         json.dump(new_data, f, ensure_ascii=False, indent=2)
+    
+    def getname(x):
+        if 'version' in x:
+            if not (x['version'] == 'DEFAULT' or x['version'] == ''):
+                return f"{x['name']}#{x['version']}"
+        return x['name']
+                
+    with open(FILE_NAME_m, 'w') as f:
+        print('Saving to names at file: ' + FILE_NAME_m)
+        f.write('\n'.join(list(sorted(map(getname, filter(lambda x: x['bonuses']['slot'] in ['head', 'torso', 'legs', 'hands', 'feet'], new_data))))))
+    with open(FILE_NAME_o, 'w') as f:
+        print('Saving to names at file: ' + FILE_NAME_o)
+        f.write('\n'.join(list(sorted(map(getname, filter(lambda x: x['bonuses']['slot'] not in ['head', 'torso', 'legs', 'hands', 'feet', 'main hand weapon', 'off-hand weapon', '2h weapon', 'off-hand'], new_data))))))
+    with open(FILE_NAME_w, 'w') as f:
+        print('Saving to names at file: ' + FILE_NAME_w)
+        f.write('\n'.join(list(sorted(map(getname, filter(lambda x: x['bonuses']['slot'] in ['main hand weapon', 'off-hand weapon', '2h weapon', 'off-hand'], new_data))))))
 
+    if not get_images:
+        return
     success_img_dls = 0
     failed_img_dls = 0
     skipped_img_dls = 0
